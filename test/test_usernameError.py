612340b443807e3f76788166a903bc1e6b47d99c
Shannon Morris <smorri55@kent.edu>
2019-11-04 16:47:24 -0500

    added login username error webdriver script

--- test/test_usernameError.py
+++ test/test_usernameError.py
@@ -13,38 +13,34 @@ driver = webdriver.Chrome(executable_path=r'C:/Program Files (x86)/Google/Chrome
 driver.get("http:localhost:5000")
 assert "Jelly Lists" in driver.title
 
-signUpButton = driver.find_element_by_id("signUpButton")
-ActionChains(driver).move_to_element(signUpButton).click().perform()
+loginButton = driver.find_element_by_id("loginButton")
+ActionChains(driver).move_to_element(loginButton).click().perform()
 
-usernameField = driver.find_element_by_id("username")
-ActionChains(driver).move_to_element(usernameField).click().perform()
-usernameField.send_keys("shaqueequee")
+#usernameField = driver.find_element_by_id("username")
+#ActionChains(driver).move_to_element(usernameField).click().perform()
+#usernameField.send_keys("shaqueequee")
 
-emailField = driver.find_element_by_name("email")
-ActionChains(driver).move_to_element(emailField).click().perform()
-emailField.send_keys("smorri55@kent.edu")
+#emailField = driver.find_element_by_name("email")
+#ActionChains(driver).move_to_element(emailField).click().perform()
+#emailField.send_keys("asdfjkl@kent.edu")
 
 passwordField = driver.find_element_by_name("password")
 ActionChains(driver).move_to_element(passwordField).click().perform()
 passwordField.send_keys("420")
+passwordField.send_keys(Keys.RETURN)
 
-password2Field = driver.find_element_by_name("password2")
-ActionChains(driver).move_to_element(password2Field).click().perform()
-password2Field.send_keys("419")
-password2Field.send_keys(Keys.RETURN)
-
-passConfirmError = driver.find_element_by_name("confirmPass")
-string1 = passConfirmError.get_attribute('innerHTML')
+usernameError = driver.find_element_by_name("usernameError")
+string1 = usernameError.get_attribute('innerHTML')
 print(string1)
 
-if(string1.find("Field must be equal to password.")) == -1:
+if(string1.find("This field is required.")) == -1:
     print("test failed.")
 else:
     print("TEST PASSED!")
 sleep(5)
 
-sleep(10)
-sleep(30)
+#sleep(10)
+#sleep(30)
 #print(emailError)
 #ActionChains(driver).move_to_element(loginButton).click().perform()
 #usernameField = driver.find_element_by_id("username")
