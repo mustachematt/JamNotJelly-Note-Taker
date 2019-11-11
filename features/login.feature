16194858d098e0836794609ebdf7b280b68be2b5
mmari13 <mmatous1@kent.edu>
2019-11-10 23:49:20 -0500

    finished login testing

--- features/login.feature
+++ features/login.feature
@@ -5,16 +5,36 @@ Feature: login correctly
     
     Scenario: Verify error for empty username field
         Given "username" field is empty
-        When I click on the "Sign In" button
-        Then I see a "This field is required" "nameError"
+        When I click on the Sign In button
+        Then I see a "This field is required" in "usernameError"
 
     Scenario: Verify error for empty password field
         Given "password" field is empty
-        When I click on the "Sign In" button
-        Then I see a "This field is required" "passwordError" 
+        When I click on the Sign In button
+        Then I see a "This field is required" in "passwordError" 
+    
+    Scenario: Get correct error for incorrect username/password combination
+        Given we type in "test1" in the "username" field
+        And we type in "wrong" in the "password" field
+        When I click on the Sign In button
+        Then I see a "Invalid username or password" in "invalidnameorpassError"
 
-
+    Scenario: Verify correct username and password works
+        Given we type in "test1" in the "username" field
+        And we type in "test" in the "password" field
+        When I click on the Sign In button
+        Then I should see the "What would you like to keep track of?"
+    
+    #current funcitnality is if you click remember me 
+    #and close out of the browser when you open browser and 
+    #navigate to site you are still logged in
+    Scenario: Verify remember me button works 1
+        Given we type in "test1" in the "username" field
+        And we type in "test" in the "password" field
+        And I click on "remember_me" field
+        Then I click on the Sign In button
+        And Close the browser
+        #When I close browser and reopen browser 
+        #And I navigate to "http://localhost:5000/mynotes"
+        #Then I should see the "What would you like to keep track of?"
         
-        
-
-
--- features/steps/loginsteps.py
+++ features/steps/loginsteps.py
@@ -1,33 +1,53 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
 from time import sleep
 
-@given(u'"username" field is empty')
+#scenario: verify error for empty username field
+
+@given(u'"{f}" field is empty')
+def step_impl(context,f):
+    #empty string are falsy so assert not false so that its true
+    #put in --no-capture on the comand line to see print statements
+    print("text in field:" + context.browser.find_element_by_id(f).get_attribute('value'))
+    assert not context.browser.find_element_by_id(f).get_attribute('value')
+
+@when(u'I click on the Sign In button')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given "username" field is empty')
+    context.browser.find_element_by_name("submit").click()
+    #can be used for a text field to click enter afterwards
+    #context.browser.find_element_by_name("value").send_keys(Keys.RETURN)
+
+@then(u'I see a "{error}" in "{field}"')
+def step_impl(context,error,field):
+    #field = context.browser.find_element_by_name(field).get_attribute("innerHTML")
+    assert error in context.browser.find_element_by_name(field).get_attribute("innerHTML")
 
 
-@when(u'I click the Sign In button')
+#use username test password test
+@given(u'we type in "{t}" in the "{f}" field')
+def step_impl(context, t, f):
+    context.browser.find_element_by_name(f).send_keys(t)
+
+
+#schenario remember me button
+@given(u'I click on "{f}" field')
+def step_impl(context,f):
+    context.browser.find_element_by_name(f).click()
+
+@then(u'I click on the Sign In button')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I click the Sign In button')
+    context.browser.find_element_by_name("submit").click()
 
 
-@then(u'I see a "This field is required" "nameError"')
+@then(u'Close the browser')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I see a "This field is required" "nameError"')
+    context.browser.close()
+    #driver = webdriver.Chrome()
 
 
-@given(u'"password" field is empty')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given "password" field is empty')
-
-
-@when(u'I click on the "Sign In" button')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When I click on the "Sign In" button')
-
-
-
-@then(u'I see a "This field is required" "passwordError"')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I see a "This field is required" "passwordError"')
+@given(u'I navigate to "{url}"')
+def step_impl(context,url):
+    
+    #context.browser=webdriver.Chrome()
+    context.browser.get(url)
