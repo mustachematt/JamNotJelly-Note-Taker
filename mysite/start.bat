3d5305201142002fc73da1beb862566fc26efa9e
[ahedgpath] <amy22_01@yahoo.com>
2019-11-17 11:31:31 -0500

    added env variables and updated gitignore

--- mysite/start.bat
+++ mysite/start.bat
@@ -1,3 +1,8 @@
 set FLASK_APP=flask_app.py
 set FLASK_DEBUG=1
+set MAIL_SERVER=smtp.googlemail.com
+set MAIL_PORT=587
+set MAIL_USE_TLS=1
+set MAIL_USERNAME=infinite.jellyloops
+set MAIL_PASSWORD=1@mj3lly
 flask run
