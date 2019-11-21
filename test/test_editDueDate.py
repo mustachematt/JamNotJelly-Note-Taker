7c5438c96b4f798eb567af838ddb8215c2623f8e
MustacheMatt <49923927+mustachematt@users.noreply.github.com>
2019-11-21 13:12:29 -0500

    Added the path to chromedriver for Travis-CI

--- test/test_editDueDate.py
+++ test/test_editDueDate.py
@@ -7,7 +7,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.action_chains import ActionChains
 
-driver = webdriver.Chrome(executable_path=r'C:/Program Files (x86)/Google/Chrome/chromedriver.exe')
+# For testing on local server
+#driver = webdriver.Chrome(executable_path=r'C:/Program Files (x86)/Google/Chrome/chromedriver.exe')
+# For testing on Travis-CI
+driver = webdriver.Chrome(executable_path=r'/home/travis/virtualenv/python3.6.7/bin/chromedriver.exe'
 
 #driver.get("https://mustachematt.pythonanywhere.com")
 driver.get("http:localhost:5000")
