5ada8d094a882802f2b9e1fd0fec764040756019
mmari13 <mmatous1@kent.edu>
2019-11-27 16:42:09 -0500

    removed path to chromedriver since its no longer there

--- test/test_editDueDate.py
+++ test/test_editDueDate.py
@@ -10,7 +10,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 # For testing on local server
 #driver = webdriver.Chrome(executable_path=r'C:/Program Files (x86)/Google/Chrome/chromedriver.exe')
 # For testing on Travis-CI
-driver = webdriver.Chrome(executable_path=r'/usr/bin/chromedriver')
+#executable_path=r'/usr/bin/chromedriver'
+driver = webdriver.Chrome()
 
 driver.get("https://mustachematt.pythonanywhere.com")
 #driver.get("http:localhost:5000")
